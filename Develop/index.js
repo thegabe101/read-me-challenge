3811c490385ac56b12553772629b5f26a1068969
Rayncor <sowa332@gmail.com>
2022-07-16 08:28:18 -0700

    added question content and comments

--- Develop/index.js
+++ Develop/index.js
@@ -1,68 +1,75 @@
 // TODO: Include packages needed for this application
 // Packages required are inquirer for sure, fs for sure, not sure what else yet 
+//need to require utils in order to interact with generatemarkdown.js
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const util = require('util');
+//const function that creates the question arrays
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // Const function that creates the question arrays 
-const generateTheMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 // These are the questions to be generated
 //Originally listed "message" as "question" before realizing that the message key was required for the inquirer prompt
 //Not sure whether to make array first or to make it inquirer.prompt method 
 
-// inquirer.prompt {[
-const questions = [{
-    name: 'Title',
+// inquirer.prompt {[]
+
+
+//building question array to be exported here
+//originally wrote name then type then message for these but that is actually confusing. better to start with overall type first before specificying id so need to change it. 
+const questions = [
+    {
     type: "Input",
+    name: 'Title',
     message: 'What is the title of your project?'
 }, {
+    type: "Input",
     name: 'Description',
-    type: "Input",
-    message: 
+    message: "How would you describe your project?"
 }, {
-    name: 'Table of Contents',
     type: "Input",
-    message: 
-}, {
     name: 'Installation',
-    type: "Input",
-    message: 
+    message: "Are there any special steps required to install your project?"
 }, {
+    type: "Input",
     name: 'Usage',
-    type: "Input",
-    message: 
+    message: "How does a user use the project you have built?"
 }, {
+    type: "Input",
     name: 'Screenshots',
-    type: "Input",
-    message: 
+    message: "Will you provide screenshots in your readme?"
 }, {
+    type: "Input",
     name: 'Credits',
-    type: "Input",
-    message: 
+    message: "Are there any credits you'd like to record in the making of your project?"
 }, {
+    type: "Input",
     name: 'License',
-    type: "Input",
-    message: 
+    message: "What license does your project use?"
 }, {
+    type: "Input",
     name: 'Badges',
-    type: "Input",
-    message: 
+    message: "What badges does your project use?"
 }, {
+    type: "Input",
     name: 'Features',
-    type: "Input",
-    message: 
+    message: "Are there any features of your project you'd like to describe?"
 }, {
+    type: "Input",
     name: 'How to Contribute',
-    type: "Input",
-    message: 
+    message: "Is there a way for people to contribute to your project's future development?"
 }, {
-    name: 'Tests',
     type: "Input",
-    message: 
+    name: 'Tests',
+    message: "Have you run any tests on your project?"
 },
 ];
+
+
+//with questions recorded the process is now focused on how we will be writing that file. however before i check the function i wrote earlier i should check what is happening in the terminal. 
 // ]};
+
+//OK, sweet- inquirer is successfully prompting the user for question inputs upon running the node. 
 
 // TODO: Create a function to write README file
 // This is just the simple function that actually writes the file, note that fs is within the write TO file function and is not the actual function itself 
